c08549fb29787ffce62f2e7bbe694cef84f4edad
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-11-13 20:43:09 +0600

    View Full  Menu button add on popular menu section

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -22,6 +22,9 @@ const PopularMenu = () => {
                     menu.map(item=> <MenuItem key={item._id} item={item}></MenuItem> )
                 }
             </div>
+         <div className="flex justify-center items-center my-8">
+         <button className="btn text-center btn-outline border-0  border-b-black border-b-4">View Full  Menu</button>
+         </div>
         </section>
     );
 }; 
