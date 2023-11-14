02323ae2ea2070c28dd38e4775f1d9de6ad41112
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-11-14 10:16:49 +0600

    add a hook for menu data fetching

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=> res.json())
-        .then(data => {
-            const popularItems = data.filter(item=> item.category === 'popular');
-            setMenu(popularItems)
-            
-        })
-    },[])
+    const [menu]= useMenu();
+    const popular = menu.filter(item=> item.category === 'popular')
+ 
     return (
         <section>
             <SectionTitle subHeading={'Check it out'} heading={'FROM OUR MENU'}></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10 my-10 ">
                 {
-                    menu.map(item=> <MenuItem key={item._id} item={item}></MenuItem> )
+                    popular.map(item=> <MenuItem key={item._id} item={item}></MenuItem> )
                 }
             </div>
          <div className="flex justify-center items-center my-8">
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,14 @@
 import HelmetTitle from "../../../Components/Helmet/HelmetTitle";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/menu-bg.jpg'
 
 
 const Menu = () => {
     return (
         <div>
             <HelmetTitle title={'Menu'}></HelmetTitle>
+            <Cover img={menuImg} title={'OUR MENU'}></Cover>
+           
         </div>
     );
 };
